b42b2f9fd8d3660803aa7a80d06576de2780b75a
Chris Eady <chris.eady1@gmail.com>
2017-07-20 14:11:23 -0500

    Change header links upon successful login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,10 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:id])
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 end

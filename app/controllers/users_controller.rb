cad779fe5cc1f15c120df9a9bc03ba9a4cc8ebbb
Chris Eady <chris.eady1@gmail.com>
2017-07-21 10:17:14 -0500

    Handle sucessful and unsuccessful edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # TODO: Handle successful update.
+      flash[:success] = 'Profile successfully updated!'
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
 
     def user_params

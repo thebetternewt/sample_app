c34432d34b867c424a837dab30422f7ffc482308
Chris Eady <chris.eady1@gmail.com>
2017-07-24 13:16:08 -0500

    Fix production.rb smtp_settings typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'pacific-citadel-66850.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settions = {
+  ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',

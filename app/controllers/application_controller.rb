4867fd60d73323548bea861509f9e816e3ca6b57
Chris Eady <chris.eady1@gmail.com>
2017-08-04 12:54:37 -0500

    Implement logged in user homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+
+    # Before filters
+
+    # Confirms a user is logged in.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url
+      end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.where(activated: true).paginate(page: params[:page])
@@ -56,19 +56,14 @@ class UsersController < ApplicationController
     end
 
     # Before filters
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = 'Please log in.'
-        redirect_to login_url
-      end
-    end
 
+    # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
     end
 
+    # Confirms an admin user.
     def admin_user
       redirect_to root_url unless current_user.admin?
     end
